268eb04e2a4e57026116359cf8bceb9ceda04d01
Andres Suarez <zertosh@gmail.com>
2016-05-08 08:50:37 -0700

    wrap regexs in functions

--- loose-detect.js
+++ loose-detect.js
@@ -4,11 +4,8 @@ var jsTokens = require('js-tokens');
 
 var searchRe = /(?:\b(?:require|import|export)\b)/;
 
-var stringRe = /^['"]/;
-var spaceOrCommentRe = /^(?:\s|[/][/*])/;
-
 module.exports = function(src) {
-  // "require" or "import" must be in the source:
+  // "require", "import" or "export" must be in the source:
   if (!searchRe.test(src)) return [];
 
   jsTokens.lastIndex = 0;
@@ -24,7 +21,7 @@ function extract(parts) {
 
     if (part === 'import' || part === 'from') {
       var afterFrom = _next(parts, i);
-      if (stringRe.test(afterFrom.token)) {
+      if (isStringToken(afterFrom.token)) {
         found.push(afterFrom.token.slice(1, -1));
         i = afterFrom.index;
         continue;
@@ -39,7 +36,7 @@ function extract(parts) {
       if (
         beforeRequire.token !== '.' &&  // not property access
         opening.token === '(' &&        // method call
-        stringRe.test(arg.token) &&     // string first arg
+        isStringToken(arg.token) &&     // string first arg
         (
           closing.token === ')' ||      // not string concat
           closing.token === ','
@@ -54,12 +51,20 @@ function extract(parts) {
   return found;
 }
 
+function isStringToken(token) {
+  return /^['"]/.test(token);
+}
+
+function isSpaceOrCommentToken(token) {
+  return /^(?:\s|[/][/*])/.test(token);
+}
+
 function _prev(parts, i) {
-  while (spaceOrCommentRe.test(parts[--i])) {}
+  while (isSpaceOrCommentToken(parts[--i]));
   return {token: parts[i], index: i};
 }
 
 function _next(parts, i) {
-  while (spaceOrCommentRe.test(parts[++i])) {}
+  while (isSpaceOrCommentToken(parts[++i]));
   return {token: parts[i], index: i};
 }
